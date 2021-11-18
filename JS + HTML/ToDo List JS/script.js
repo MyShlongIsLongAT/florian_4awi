9f539565a6e5accb72114e954cf3dd83a7fada00
Florian <72039636+MyShlongIsLongAT@users.noreply.github.com>
2021-11-18 20:15:39 +0100

    added ToDo-List

--- JS + HTML/ToDo List JS/script.js
+++ JS + HTML/ToDo List JS/script.js
@@ -1,3 +1,40 @@
+//var taskList = ["kochen", "sosi hü", "chemie lerna"];
+
+var taskList = {
+    "tasks": [
+
+    ]
+
+};
+
+printToDoList();
+
 function addTask() {
-    alert("lel");
+    let newTask = document.getElementById("inputTask").value;
+    let newCreator = document.getElementById("inputCreator").value;
+
+    taskList["tasks"].push({ "taskName": newTask, "taskCreator": newCreator, "taskStatus": false });
+    printToDoList();
+}
+
+function printToDoList() {
+    document.getElementById("tasks").innerHTML = getHTMLforToDoList();
+}
+
+function getHTMLforToDoList() {
+    document.getElementById("tasks").innerHTML = "";
+    let html = "";
+    for (let i = 0; i < taskList.tasks.length; i++) {
+        let checked = "";
+        if (taskList.tasks[i].taskStatus) {
+            checked = "checked";
+        }
+        html += "<li><input onClick='setCheckboxStatus(this)' class='checkbox' type='checkbox' data-index='" + i + "'" + checked + "/>" + taskList.tasks[i].taskName + " - " + taskList.tasks[i].taskCreator + "</li>";
+    }
+    return html;
+}
+
+function setCheckboxStatus(checkBoxElement) {
+    elementIndex = checkBoxElement.attributes["data-index"].value;
+    taskList.tasks[elementIndex].taskStatus = checkBoxElement.checked;
 }
