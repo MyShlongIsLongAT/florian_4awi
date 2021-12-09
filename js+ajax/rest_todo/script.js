36014d5369c40dc0768891d1f6e1208510ba2bdb
MyShlongIsLongAT <florian_breuker@outlook.com>
2021-12-09 08:42:59 +0100

    started rest_todo

---
+++ js+ajax/rest_todo/script.js
@@ -0,0 +1,15 @@
+function registerEvents() {
+    document.getElementById("loadTasks").addEventListener("click", function() {
+        alert("HS");
+    })
+
+    document.getElementById("loadUsers").addEventListener("click", function() {
+        alert("lul");
+    })
+}
+
+function loadTasks() {
+
+}
+
+registerEvents();
